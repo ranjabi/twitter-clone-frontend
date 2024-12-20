e3c60514674bdca0a2f4c70e548db351087a771c
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-21 06:44:52 +0700

    chore: sample page

---
+++ src/pages/register.tsx
@@ -0,0 +1,19 @@
+const Register = () => {
+  // const getUser = async () => {
+  //   try {
+  //     const res = await axios.get(`${process.env.API_BASE_URL}/health-check`)
+  //     console.log(res)
+  //   }
+  //   catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+
+  // getUser()
+
+  return (
+    <div className="container mx-auto px-4 max-w-screen-md">register page</div>
+  )
+}
+
+export default Register
