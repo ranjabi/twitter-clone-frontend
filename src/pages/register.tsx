2b294d7a8db06ee238fe9c88974625d85658edae
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-21 06:49:30 +0700

    chore: sample page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,15 +1,16 @@
-const Register = () => {
-  // const getUser = async () => {
-  //   try {
-  //     const res = await axios.get(`${process.env.API_BASE_URL}/health-check`)
-  //     console.log(res)
-  //   }
-  //   catch (error) {
-  //     console.log(error)
-  //   }
-  // }
+import axios from 'axios'
 
-  // getUser()
+const Register = () => {
+  const getUser = async () => {
+    try {
+      const res = await axios.get('api/health-check')
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  getUser()
 
   return (
     <div className="container mx-auto px-4 max-w-screen-md">register page</div>
