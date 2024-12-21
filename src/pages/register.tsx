44baa0eb795b565e05725a9bf6b4d8ae637de76e
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-21 08:10:34 +0700

    feat: react hook

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,26 +1,46 @@
+import { apiInstance } from '@/lib/utils'
 import axios from 'axios'
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const Register = () => {
-  const [data, setData] = useState('')
-  const getUser = async () => {
+  // const [data, setData] = useState('')
+  const { register, handleSubmit } = useForm()
+  const onSubmit = (data) => {
+    getUser(data)
+    console.log(data)
+  }
+
+  const getUser = async (reqBody) => {
+    // const reqBody = {
+    //   username: '',
+    //   email: '',
+    //   password: '',
+    // }
     try {
-      const res = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/health-check`,
-      )
-      setData(JSON.stringify(res.data))
+      const res = await apiInstance.post('/register', reqBody)
+      // setData(JSON.stringify(res.data))
       console.log(res)
     } catch (error) {
+      if (axios.isAxiosError(error)) {
+        // do something
+        // or just re-throw the error
+        console.log('msg:', error.message)
+        console.log('code:', error.code)
+        console.log('res:', error.response?.data.message)
+      }
       console.log(error)
     }
   }
 
-  getUser()
-
   return (
     <div className="container mx-auto px-4 max-w-screen-md">
       <p>register page</p>
-      <p>{data || 'null'}</p>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
+        <input {...register('username')} />
+        <input {...register('email')} />
+        <input {...register('password')} />
+        <input type="submit" />
+      </form>
     </div>
   )
 }
