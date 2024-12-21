08f0908db716df384d213f9561b62409f398f7b2
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-21 07:24:41 +0700

    chore: sample page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,7 +3,9 @@ import axios from 'axios'
 const Register = () => {
   const getUser = async () => {
     try {
-      const res = await axios.get('api/health-check')
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/health-check`,
+      )
       console.log(res)
     } catch (error) {
       console.log(error)
