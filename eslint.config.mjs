275db328fdebad66c51d1435b6eb56aa26e148e3
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-21 08:19:49 +0700

    feat: shadcn init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  eslintPluginPrettierRecommended
+  eslintPluginPrettierRecommended,
+  
 ];
 
 export default eslintConfig;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
-const apiInstance = axios.create({
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const apiInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
 })
-
-export { apiInstance }
