30fab163374e40c1a8e0a2dd44de4853cb347f65
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-21 07:28:56 +0700

    chore: sample page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,11 +1,14 @@
 import axios from 'axios'
+import { useState } from 'react'
 
 const Register = () => {
+  const [data, setData] = useState('')
   const getUser = async () => {
     try {
       const res = await axios.get(
         `${process.env.NEXT_PUBLIC_API_BASE_URL}/health-check`,
       )
+      setData(JSON.stringify(res.data))
       console.log(res)
     } catch (error) {
       console.log(error)
@@ -15,7 +18,10 @@ const Register = () => {
   getUser()
 
   return (
-    <div className="container mx-auto px-4 max-w-screen-md">register page</div>
+    <div className="container mx-auto px-4 max-w-screen-md">
+      <p>register page</p>
+      <p>{data || 'null'}</p>
+    </div>
   )
 }
 
